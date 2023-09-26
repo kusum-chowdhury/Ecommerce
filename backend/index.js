dbaf1c863c6d7e5abbe6a2699e6dbe4f4ae724bc
kusum chowdhury <kusumchowdhury5832@gmail.com>
2023-09-26 15:08:51 -0700

    create server

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.listen(PORT, ()=> {
+    console.log('server is running')
+})

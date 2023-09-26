83e6e1284f97f256415b49a95f77b729a0e5469c
kusum chowdhury <kusumchowdhury5832@gmail.com>
2023-09-26 15:23:43 -0700

    create endpoint for auth router

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
+//import routes file
+const authRouter = require('./routes/authRouter');
 const PORT = 3000;
 
 
 //middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
+// Use the routers in the application
+app.use('/auth', authRouter);
+
 
 app.listen(PORT, ()=> {
     console.log('server is running')

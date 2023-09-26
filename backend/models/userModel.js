153f687c3096b1c1900239c8ce904b5fb82b698b
kusum chowdhury <kusumchowdhury5832@gmail.com>
2023-09-26 16:17:36 -0700

    create schema for user

---
+++ backend/models/userModel.js
@@ -0,0 +1,20 @@
+const {createDB} = require('../config/db');
+const {DataTypes} = require('sequelize');
+
+const User = createDB.define('user', {
+    id: {
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+        type: DataTypes.INTEGER,
+    },
+        name: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password: DataTypes.STRING,
+        isSeller: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false,
+        }
+})
+
+module.exports = User;

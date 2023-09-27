37aae43d4fe14929b2ecdbcb4c41cfe63319aa67
kusum chowdhury <kusumchowdhury5832@gmail.com>
2023-09-27 15:30:27 -0700

    add login functionality for user

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,8 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/userModel');
+const jwt = require('jsonwebtoken')
 
+//function to register user
 const registerController = async(req, res)=> {
    try{
     const { name, email, password, isSeller } = req.body;
@@ -14,4 +16,30 @@ const registerController = async(req, res)=> {
     return res.status(500).json({err: e});
    }
 }
-module.exports = registerController
+
+//login funtionality
+const loginController = async(req, res)=> {
+    try{
+        const {email, password} = req.body;
+        const userExists = await User.findOne({where: {email}});
+        if(!userExists){
+         return res.status(401).json({message: "user not found"});
+        }
+        const passwordMatched = bcrypt.compareSync(password, userExists.password);
+       if(!passwordMatched){
+         return res.status(401).json({Message: "incorrect password or email"})
+       }
+       const payload = {user: {id: userExists.id}};
+       const token = await jwt.sign(payload, "secret message", {
+         expiresIn: 450000,
+       } )
+       res.cookie("t", token, {expire: new Date() + 10000 });
+       console.log("success")
+       return res.status(200).json({ token });
+    }catch(e){
+        console.log(">>", e)
+        return res.status(500).send(e);
+    }
+}
+
+module.exports ={ registerController, loginController}
--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const registerController = require('../controllers/authController')
+const {registerController, loginController} = require('../controllers/authController')
 const {userValidationRules, validate} = require('../validators/validator')
 
 
 router.post('/register', userValidationRules(), validate, registerController);
-// router.post('/login', loginController);
+router.post('/login', loginController);
 
 module.exports = router;
